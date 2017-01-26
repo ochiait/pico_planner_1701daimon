f74bb3d06c44dcf473423894908375d9603092fe
Tetsuya OCHIAI <ochiai@ryorika-mm.jp>
2017-01-26 18:31:00 +0900

    usersコントローラー作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+  # 新規作成フォーム
+  def new
+    @user = User.new
+  end
+
+  # ユーザの新規登録
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user, notice: "ユーザを登録しました。"
+    else
+      render "new"
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
 end
