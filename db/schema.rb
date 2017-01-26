54abee1c7c1f43f67f29b1fc70ad6326b8843b18
Tetsuya OCHIAI <ochiai@ryorika-mm.jp>
2017-01-26 18:09:05 +0900

    マイグレーション
    rails db:migrate RAILS_ENV=development

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160813144526) do
+ActiveRecord::Schema.define(version: 20170126070234) do
 
   create_table "plan_items", force: :cascade do |t|
     t.string   "name",        null: false
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20160813144526) do
     t.datetime "ends_at",     null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",            null: false
+    t.string   "hashed_password"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
