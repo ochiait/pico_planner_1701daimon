7a7b1dc1ff4a745934e5ac689a9262165e197653
Tetsuya OCHIAI <ochiai@ryorika-mm.jp>
2017-02-10 23:29:59 +0900

    予定とユーザーの関連付け（６）plan_itemsテーブルにuser_idカラムをインデックスとして追加

--- db/migrate/20170210071229_add_user_id_to_plan_items.rb
+++ db/migrate/20170210071229_add_user_id_to_plan_items.rb
@@ -1,5 +1,5 @@
 class AddUserIdToPlanItems < ActiveRecord::Migration[5.0]
   def change
-    add_column :plan_items, :user_id, :integer
+    add_reference :plan_items, :user, index: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170210071229) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.index ["user_id"], name: "index_plan_items_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
