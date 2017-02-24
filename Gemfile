5973728ef98b8f64ff4945e149d3fa9fd7026768
Tetsuya OCHIAI <ochiai@ryorika-mm.jp>
2017-02-24 13:53:52 +0900

    rails-erd(ER図生成)

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ group :development do
   gem 'annotate'
   gem 'better_errors'
   gem 'binding_of_caller'
+  gem 'rails-erd' 
 end
 
 group :test do
--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: participations
+#
+#  id           :integer          not null, primary key
+#  user_id      :integer          not null
+#  plan_item_id :integer          not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Participation < ApplicationRecord
   belongs_to :user
   belongs_to :plan_item
